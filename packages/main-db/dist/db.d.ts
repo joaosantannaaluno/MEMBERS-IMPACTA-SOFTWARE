5ba9cf0f24002dd1f432351541f34aa9f8cbac26
João Victor Sant'Anna <joao.victor@ubus.com.br>
2025-03-01 13:28:05 -0300

    Adiciona a pasta dist para remoção

--- packages/main-db/dist/db.d.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-export declare const dbClient: import("drizzle-orm/libsql", { with: { "resolution-mode": "import" } }).LibSQLDatabase<Record<string, never>> & {
-    $client: import("@libsql/client", { with: { "resolution-mode": "import" } }).Client;
-};
-//# sourceMappingURL=db.d.ts.map
